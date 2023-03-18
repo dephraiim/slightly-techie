76978445b7047e46370e3a396782fa9d1bbf261f
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-03-18 17:19:57 +0000

    Update post page complete

--- src/pages/edit/[id].tsx
+++ src/pages/edit/[id].tsx
@@ -7,6 +7,8 @@ import { createTRPCContext } from "~/server/api/trpc";
 import { appRouter } from "~/server/api/root";
 import superjson from "superjson";
 import { api as trpc } from "~/utils/api";
+import React from "react";
+import Link from "next/link";
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext<{ id: string }>
@@ -36,17 +38,78 @@ export default function EditPage(
   const postQuery = trpc.posts.getPost.useQuery({ id });
   const { data } = postQuery;
 
+  const [title, setTitle] = React.useState(data?.title);
+  const [content, setContent] = React.useState(data?.content);
+
   if (!data) {
-    return <div>Loading...</div>;
+    return <div className="p-6">Loading...</div>;
   }
 
+  const updatePost = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await Promise.resolve("Update post");
+
+    const input = {
+      title,
+      content,
+    };
+  };
+
   return (
-    <>
-      <h1>{data.title}</h1>
+    <main className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] p-6 text-white">
+      <h1 className="text-4xl">Edit post</h1>
+      {/*
       <em>Created {data.createdAt.toLocaleDateString()}</em>
       <p>{data.content}</p>
       <h2>Raw data:</h2>
-      <pre>{JSON.stringify(data, null, 4)}</pre>
-    </>
+      <pre>{JSON.stringify(data, null, 4)}</pre> */}
+
+      <form onSubmit={updatePost}>
+        <label className="text-white" htmlFor="title">
+          Title:
+        </label>
+        <br />
+        <input
+          id="title"
+          name="title"
+          type="text"
+          value={title}
+          className="text-black"
+          onChange={(e) => setTitle(e.target.value)}
+          // disabled={addPost.isLoading}
+        />
+
+        <br />
+        <label className="text-white" htmlFor="text">
+          Content:
+        </label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          value={content}
+          className="text-black"
+          onChange={(e) => setContent(e.target.value)}
+          // disabled={addPost.isLoading}
+        />
+        <br />
+        <input
+          className="my-2 mr-2 rounded-full bg-white/10 p-1 px-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          type="submit"
+          value="Update"
+          // disabled={addPost.isLoading}
+        />
+        {/* {addPost.error && (
+            <p style={{ color: "red" }}>{addPost.error.message}</p>
+          )} */}
+        <Link
+          className="text-b my-2 mr-2 rounded-full bg-red-400 p-1 px-3 font-semibold no-underline transition hover:bg-white/20"
+          href="/"
+        >
+          Cancel
+        </Link>
+      </form>
+    </main>
   );
 }
